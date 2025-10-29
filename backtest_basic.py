c77bd1991a5dd28a03ddc95c0ee0e3c177e2c496
YixinChen <Tonyyiixn@gmail.com>
2025-10-29 11:33:08 +0800

    rewrite some of Tdx style function and applied a simple buy&sell strategy of Williams%R

--- backtest_basic.py
+++ backtest_basic.py
@@ -17,6 +17,62 @@ def tdx_sma(price_list,N,M):
             sma_list.append(sma_value)
     return sma_list
 
+def tdx_ema(price_list, N):
+    """
+    This is a function to calculate the TDX-style Exponential Moving Average (EMA).
+    price_list: the list of prices 
+    N: the period of the EMA
+    return: a list of EMA values
+    """
+    ema_list = []
+    multiplier = 2 / (N + 1)
+    for i in range(len(price_list)):
+        if i == 0:
+            ema_list.append(price_list[0])
+        else:
+            ema_value = (price_list[i] - ema_list[i - 1]) * multiplier + ema_list[i - 1]
+            ema_list.append(ema_value)
+    return ema_list
+
+def tdx_ma(price_list, N):
+    """
+    This is a function to calculate the TDX-style Moving Average (MA).
+    price_list: the list of prices 
+    N: the period of the MA
+    return: a list of MA values
+    """
+    ma_list = []
+    for i in range(len(price_list)):
+        if i < N - 1:
+            ma_list.append(sum(price_list[:i + 1]) / (i + 1))
+        else:
+            ma_value = sum(price_list[i - N + 1:i + 1]) / N
+            ma_list.append(ma_value)
+    return ma_list
+
+class LWR_Indicator(bt.Indicator):
+    lines = ('lwr1','lwr2')
+    params = (('N', 14),('M1', 3),('M2', 3),)
+
+    def __init__(self):
+        self.addminperiod(self.params.N)
+        self.rsv_list = []
+    def next(self):
+        highest_high = max(self.data.high.get(size=self.params.N))
+        lowest_low = min(self.data.low.get(size=self.params.N))
+        if highest_high == lowest_low:
+            rsv = 0
+        else:
+            rsv = (highest_high - self.data.close[0]) / (highest_high - lowest_low) * 100
+        
+        self.rsv_list.append(rsv)
+
+        lwr1_list = tdx_sma(self.rsv_list, self.params.M1, 1)
+        lwr2_list = tdx_sma(lwr1_list, self.params.M2, 1)   
+        
+        self.lines.lwr1[0] = lwr1_list[-1]
+        self.lines.lwr2[0] = lwr2_list[-1]
+
 class TestStrategy(bt.Strategy):
     def __init__(self):
         pass
@@ -25,18 +81,30 @@ class TestStrategy(bt.Strategy):
         # runs every bar
         print(f'Close: {self.data.close[0]}')
 
-class RSI_Strategy(bt.Strategy):
-    params = (('rsi_period', 14), ('rsi_overbought', 70), ('rsi_oversold', 30),)
+class LWR_Strategy(bt.Strategy):
+    params = (('N', 14), ('M1', 3), ('M2', 3))
 
     def __init__(self):
-        self.rsi = bt.indicators.RSI(self.data.close, period=self.params.rsi_period)
+        self.lwr = LWR_Indicator(
+            self.data, 
+            N=self.params.N, 
+            M1=self.params.M1, 
+            M2=self.params.M2
+        )
 
     def next(self):
+        close = self.data.close[0]
+        lwr1 = self.lwr.lwr1[0]
+        lwr2 = self.lwr.lwr2[0]
+
+        if lwr1 is None or lwr2 is None:
+            return
+        
         if not self.position:
-            if self.rsi < 30:
+            if lwr2 < 20:
                 self.buy()
         else:
-            if self.rsi > 70:
+            if lwr2 > 80:
                 self.sell()
 
 cerebro = bt.Cerebro()
@@ -57,7 +125,7 @@ data = bt.feeds.GenericCSVData(
 )
 
 cerebro.adddata(data)
-cerebro.addstrategy(RSI_Strategy)
+cerebro.addstrategy(LWR_Strategy)
 cerebro.run()
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())    
 cerebro.plot()
